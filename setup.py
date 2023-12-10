09114887f8240229115e921812a30186d7e66cee
bramantyois <bramantyo.supriyatno.15@gmail.com>
2023-12-10 17:46:30 +0100

    setup. updated

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    long_description = f.read() 
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
 setup(
-    name='nkss-simulator',
-    version='0.1',
+    name='nkss_simulator',
+    version='0.0.1',
+    author='Bramantyo Supriyatno',
+    url='https://github.com/bramantyois/nkss',
     packages=find_packages(),  # Automatically find and include all packages
-    install_requires=[
-        'numpy',
-        'tqdm'
-    ],
+    install_requires=requirements,
+    long_description=long_description,
 )
